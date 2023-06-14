af4663b141997bc7deac293e82d287aecd2aba12
maciejm2517 <maciej.konto.1357@gmail.com>
2023-06-14 20:23:17 +0200

    pose_new

--- src/utilities_pose.js
+++ src/utilities_pose.js
@@ -2,61 +2,10 @@ import * as posenet from "@tensorflow-models/posenet";
 import * as tf from "@tensorflow/tfjs";
 
 const color = "aqua";
-const boundingBoxColor = "red";
 const lineWidth = 2;
 
 export const tryResNetButtonName = "tryResNetButton";
 export const tryResNetButtonText = "[New] Try ResNet50";
-const tryResNetButtonTextCss = "width:100%;text-decoration:underline;";
-const tryResNetButtonBackgroundCss = "background:#e61d5f;";
-
-function isAndroid() {
-  return /Android/i.test(navigator.userAgent);
-}
-
-function isiOS() {
-  return /iPhone|iPad|iPod/i.test(navigator.userAgent);
-}
-
-export function isMobile() {
-  return isAndroid() || isiOS();
-}
-
-function setDatGuiPropertyCss(propertyText, liCssString, spanCssString = "") {
-  var spans = document.getElementsByClassName("property-name");
-  for (var i = 0; i < spans.length; i++) {
-    var text = spans[i].textContent || spans[i].innerText;
-    if (text == propertyText) {
-      spans[i].parentNode.parentNode.style = liCssString;
-      if (spanCssString !== "") {
-        spans[i].style = spanCssString;
-      }
-    }
-  }
-}
-
-export function updateTryResNetButtonDatGuiCss() {
-  setDatGuiPropertyCss(
-    tryResNetButtonText,
-    tryResNetButtonBackgroundCss,
-    tryResNetButtonTextCss
-  );
-}
-
-
-export function toggleLoadingUI(
-  showLoadingUI,
-  loadingDivId = "loading",
-  mainDivId = "main"
-) {
-  if (showLoadingUI) {
-    document.getElementById(loadingDivId).style.display = "block";
-    document.getElementById(mainDivId).style.display = "none";
-  } else {
-    document.getElementById(loadingDivId).style.display = "none";
-    document.getElementById(mainDivId).style.display = "block";
-  }
-}
 
 function toTuple({ y, x }) {
   return [y, x];
@@ -110,72 +59,3 @@ export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
     drawPoint(ctx, y * scale, x * scale, 3, color);
   }
 }
-
-export function drawBoundingBox(keypoints, ctx) {
-  const boundingBox = posenet.getBoundingBox(keypoints);
-
-  ctx.rect(
-    boundingBox.minX,
-    boundingBox.minY,
-    boundingBox.maxX - boundingBox.minX,
-    boundingBox.maxY - boundingBox.minY
-  );
-
-  ctx.strokeStyle = boundingBoxColor;
-  ctx.stroke();
-}
-
-
-export async function renderToCanvas(a, ctx) {
-  const [height, width] = a.shape;
-  const imageData = new ImageData(width, height);
-
-  const data = await a.data();
-
-  for (let i = 0; i < height * width; ++i) {
-    const j = i * 4;
-    const k = i * 3;
-
-    imageData.data[j + 0] = data[k + 0];
-    imageData.data[j + 1] = data[k + 1];
-    imageData.data[j + 2] = data[k + 2];
-    imageData.data[j + 3] = 255;
-  }
-
-  ctx.putImageData(imageData, 0, 0);
-}
-
-export function renderImageToCanvas(image, size, canvas) {
-  canvas.width = size[0];
-  canvas.height = size[1];
-  const ctx = canvas.getContext("2d");
-
-  ctx.drawImage(image, 0, 0);
-}
-
-
-export function drawHeatMapValues(heatMapValues, outputStride, canvas) {
-  const ctx = canvas.getContext("2d");
-  const radius = 5;
-  const scaledValues = heatMapValues.mul(tf.scalar(outputStride, "int32"));
-
-  drawPoints(ctx, scaledValues, radius, color);
-}
-
-
-function drawPoints(ctx, points, radius, color) {
-  const data = points.buffer().values;
-
-  for (let i = 0; i < data.length; i += 2) {
-    const pointY = data[i];
-    const pointX = data[i + 1];
-
-    if (pointX !== 0 && pointY !== 0) {
-      ctx.beginPath();
-      ctx.arc(pointX, pointY, radius, 0, 2 * Math.PI);
-      ctx.fillStyle = color;
-      ctx.fill();
-    }
-  }
-}
-
