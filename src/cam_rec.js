4fd37c2f3f40dd52306c7c26052748ffc300160c
maciej <maciej.konto.1357@gmail.com>
2024-01-13 22:05:22 +0100

    bootstrap obj det

--- src/cam_rec.js
+++ src/cam_rec.js
@@ -5,7 +5,8 @@ import Webcam from "react-webcam";
 import "./App.css";
 import { nextFrame } from "@tensorflow/tfjs";
 // 2. TODO - Import drawing utility here
-import {drawRect} from "./utilities_cam_rec"; 
+import { drawRect } from "./utilities_cam_rec";
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   const webcamRef = useRef(null);
@@ -44,21 +45,21 @@ function App() {
 
       // 4. TODO - Make Detections
       const img = tf.browser.fromPixels(video)
-      const resized = tf.image.resizeBilinear(img, [640,480])
+      const resized = tf.image.resizeBilinear(img, [640, 480])
       const casted = resized.cast('int32')
       const expanded = casted.expandDims(0)
       const obj = await net.executeAsync(expanded)
-      
+
       const boxes = await obj[5].array()
       const classes = await obj[7].array()
       const scores = await obj[1].array()
-    
+
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
       // 5. TODO - Update drawing utility
       // drawSomething(obj, ctx)  
-      requestAnimationFrame(()=>{drawRect(boxes[0], classes[0], scores[0], 0.4, videoWidth, videoHeight, ctx)}); 
+      requestAnimationFrame(() => { drawRect(boxes[0], classes[0], scores[0], 0.8, videoWidth, videoHeight, ctx) });
 
       tf.dispose(img)
       tf.dispose(resized)
@@ -69,43 +70,36 @@ function App() {
     }
   };
 
-  useEffect(()=>{runCoco()},[]);
+  useEffect(() => { runCoco() }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          muted={true} 
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
+    <Container>
+      <Row>
+        <Col>
+          <Webcam
+            ref={webcamRef}
+            muted={true}
+            style={{
+              position: "absolute",
+              zindex: 8,
+              width: 640,
+              height: 480,
+            }}
+          />
 
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 8,
-            width: 640,
-            height: 480,
-          }}
-        />
-      </header>
-    </div>
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              zindex: 8,
+              width: 640,
+              height: 480,
+            }}
+          />
+        </Col>
+      </Row>
+
+    </Container>
   );
 }
 
